9700c03d512682ec9a35fc488a89cd00b488c7ee
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-02-01 00:10:30 +0000

    Very basic movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+
+    private Vector2 moveDirection = Vector2.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector2
+            (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
