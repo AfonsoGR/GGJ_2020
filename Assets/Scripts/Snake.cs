421b312e5fcd480446797dbcbd48b18a7c1b267d
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2020-02-02 03:20:30 +0000

    Snake body movement

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -7,9 +7,13 @@ public class Snake : MonoBehaviour
     private const float _PARTS_OFFSET = .56f;
 
     [SerializeField] private byte _snakeLength = 8;
+    [SerializeField] private float _partSpeed = 1.0f;
+    [SerializeField] private Transform _head;
 
-    [SerializeField] private GameObject _snakeBody = null; 
+    [SerializeField] private GameObject _snakeBody = null;
     [SerializeField] private GameObject _snakeTail = null; 
+
+    private GameObject _tail;
 
     private List<GameObject> _snakeCollection;
 
@@ -29,6 +33,12 @@ public class Snake : MonoBehaviour
         SpawnBodyParts();
     }
 
+    void Update()
+    {
+        //UpdatePosition();
+        UpdatePartRotation();
+    }
+
     private void SpawnBodyParts()
     {
         _snakeCollection.Add(
@@ -43,6 +53,51 @@ public class Snake : MonoBehaviour
     
     private void SpawnTail()
     {
-        Instantiate(_snakeTail, NextPartPosition, Quaternion.identity, transform);
+        _tail = Instantiate
+            (_snakeTail, NextPartPosition, Quaternion.identity, transform);
+    }
+
+    private void UpdatePartRotation()
+    {
+        GameObject segment;
+        Vector3 tempRot;
+        Vector3 positionS;
+        Vector3 targetS;
+        Vector3 diff;
+        for (int i = 0; i < _snakeCollection.Count; i++)
+        {
+            segment = _snakeCollection[i];
+            positionS = _snakeCollection[i].transform.position;
+            targetS = i == 0 ? _head.position : _snakeCollection[i - 1].transform.position;
+
+            _snakeCollection[i].transform.rotation = Quaternion.LookRotation(Vector3.forward, (targetS - positionS).normalized);
+            
+            tempRot = _snakeCollection[i].transform.rotation.eulerAngles;
+            tempRot.Set(tempRot.x, tempRot.y, tempRot.z + 90);
+
+            _snakeCollection[i].transform.rotation = Quaternion.Euler(tempRot);
+            diff = positionS - targetS;
+            diff.Normalize();
+
+            if (i == 0)
+                segment.transform.position = targetS;
+            else
+                segment.transform.position = targetS + _PARTS_OFFSET * diff;
+        }
+
+        segment = _tail;
+        positionS = _tail.transform.position;
+        targetS = _snakeCollection[_snakeCollection.Count - 1].transform.position;
+
+        _tail.transform.rotation = Quaternion.LookRotation(Vector3.forward, (targetS - positionS).normalized);
+            
+        tempRot = _tail.transform.rotation.eulerAngles;
+        tempRot.Set(tempRot.x, tempRot.y, tempRot.z + 90);
+
+        _tail.transform.rotation = Quaternion.Euler(tempRot);
+        diff = positionS - targetS;
+        diff.Normalize();
+
+        segment.transform.position = targetS + _PARTS_OFFSET * diff;
     }
 }
