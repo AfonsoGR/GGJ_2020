d69e394d5c99bed9102218f4c1c641481ea3ce39
Miguel Fernandez <miguelrf95@gmail.com>
2020-02-02 09:12:39 +0000

    Early stage of asteroid movement. Black hole mechanic attracts other game objects

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,6 +4,7 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = default;
     private int _dmg = default;
+    private Rigidbody2D _rb = default;
 
     public int Dmg => _dmg;
 
@@ -19,6 +20,16 @@ public class Asteroid : MonoBehaviour
         {
             _dmg = 30;
         }
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        _rb.AddForce(new Vector2(_moveSpeed, _moveSpeed));
     }
 
     // TODO: MOVEMENT
---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class BlackHole : MonoBehaviour
+{
+    [SerializeField] private float _GRAVITY_PULL = default;
+    private float m_GravityRadius = default;
+    private void Awake()
+    {
+        m_GravityRadius = GetComponent<CircleCollider2D>().radius;
+    }
+
+    /// <summary>
+    /// Attract objects towards an area when they come within the bounds of a collider.
+    /// This function is on the physics timer so it won't necessarily run every frame.
+    /// </summary>
+    /// <param name="other">Any object within reach of gravity's collider</param>
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        print(other.tag);
+
+        // if (other.attachedRigidbody)
+        // {
+            float gravityIntensity = Vector2.Distance(
+                transform.position, other.transform.position) / m_GravityRadius;
+            
+            other.attachedRigidbody.AddForce(
+                (transform.position - other.transform.position)
+                * gravityIntensity 
+                * other.attachedRigidbody.mass 
+                * _GRAVITY_PULL 
+                * Time.smoothDeltaTime);
+            
+            Debug.DrawRay(
+                other.transform.position,
+                transform.position - other.transform.position);
+        //}
+    }
+}
